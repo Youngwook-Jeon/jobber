1f34a721ae15ff9a7e84f6148e07d0501fb5178a
Youngwook-Jeon <ela87@naver.com>
2024-05-07 18:56:01 +0900

    forgot/reset/change password

--- services/auth-service/src/routes/auth.ts
+++ services/auth-service/src/routes/auth.ts
@@ -2,7 +2,7 @@ import express, { Router } from 'express';
 import { create } from '@auth/controllers/signup';
 import { read } from '@auth/controllers/signin';
 import { update } from '@auth/controllers/verify-email';
-// import { changePassword, forgotPassword, resetPassword } from '@auth/controllers/password';
+import { changePassword, forgotPassword, resetPassword } from '@auth/controllers/password';
 // import { updateOTP } from '@auth/controllers/verify-otp';
 
 const router: Router = express.Router();
@@ -11,10 +11,10 @@ export function authRoutes(): Router {
   router.post('/signup', create);
   router.post('/signin', read);
   router.put('/verify-email', update);
+  router.put('/forgot-password', forgotPassword);
+  router.put('/reset-password/:token', resetPassword);
+  router.put('/change-password', changePassword);
   //   router.put('/verify-otp/:otp', updateOTP);
-  //   router.put('/forgot-password', forgotPassword);
-  //   router.put('/reset-password/:token', resetPassword);
-  //   router.put('/change-password', changePassword);
 
   return router;
 }
--- services/gateway-service/src/routes/auth.ts
+++ services/gateway-service/src/routes/auth.ts
@@ -2,7 +2,7 @@ import express, { Router } from 'express';
 import { SignUp } from '@gateway/controllers/auth/signup';
 import { SignIn } from '@gateway/controllers/auth/signin';
 import { VerifyEmail } from '@gateway/controllers/auth/verify-email';
-// import { Password } from '@gateway/controllers/auth/password';
+import { Password } from '@gateway/controllers/auth/password';
 // import { AuthSeed } from '@gateway/controllers/auth/seed';
 // import { Signout } from '@gateway/controllers/auth/signout';
 // import { VerifyOTP } from '@gateway/controllers/auth/verify-otp';
@@ -19,10 +19,10 @@ class AuthRoutes {
     this.router.post('/auth/signin', SignIn.prototype.read);
     // this.router.post('/auth/signout', Signout.prototype.update);
     this.router.put('/auth/verify-email', VerifyEmail.prototype.update);
+    this.router.put('/auth/forgot-password', Password.prototype.forgotPassword);
+    this.router.put('/auth/reset-password/:token', Password.prototype.resetPassword);
+    this.router.put('/auth/change-password', Password.prototype.changePassword);
     // this.router.put('/auth/verify-otp/:otp', VerifyOTP.prototype.update);
-    // this.router.put('/auth/forgot-password', Password.prototype.forgotPassword);
-    // this.router.put('/auth/reset-password/:token', Password.prototype.resetPassword);
-    // this.router.put('/auth/change-password', Password.prototype.changePassword);
     // this.router.put('/auth/seed/:count', AuthSeed.prototype.create);
     return this.router;
   }
