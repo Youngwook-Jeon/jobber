e134723f3c6da2012fc85aeab2171c31c1dcdebb
Youngwook-Jeon <ela87@naver.com>
2024-05-10 21:34:36 +0900

    Show static data of categories

--- jobber-client/src/features/index/Index.tsx
+++ jobber-client/src/features/index/Index.tsx
@@ -1,6 +1,9 @@
 import { FC, LazyExoticComponent, ReactElement, Suspense, lazy } from 'react';
 import { IHeader } from 'src/shared/header/interfaces/header.interface';
 import Hero from './Hero';
+import GigTabs from './gig-tabs/GigTabs';
+import HowItWorks from './HowItWorks';
+import Categories from './Categories';
 
 const IndexHeader: LazyExoticComponent<FC<IHeader>> = lazy(() => import('src/shared/header/components/Header'));
 
@@ -10,6 +13,10 @@ const Index: FC = (): ReactElement => {
       <Suspense>
         <IndexHeader navClass="navbar peer-checked:navbar-active fixed z-20 w-full border-b border-gray-100 bg-white/90 shadow-2xl shadow-gray-600/5 backdrop-blur dark:border-gray-800 dark:bg-gray-900/80 dark:shadow-none" />
         <Hero />
+        <GigTabs />
+        <HowItWorks />
+        <hr />
+        <Categories />
       </Suspense>
     </div>
   );
