3272689c0d0771d49a830afbd134bae3144b904c
Youngwook-Jeon <ela87@naver.com>
2024-05-17 21:27:37 +0900

    Add the order service

--- services/gateway-service/src/server.ts
+++ services/gateway-service/src/server.ts
@@ -22,7 +22,7 @@ import { axiosSellerInstance } from '@gateway/services/api/seller.service';
 import { axiosGigInstance } from '@gateway/services/api/gig.service';
 import { SocketIOAppHandler } from '@gateway/sockets/socket';
 import { axiosMessageInstance } from '@gateway/services/api/message.service';
-// import { axiosOrderInstance } from '@gateway/services/api/order.service';
+import { axiosOrderInstance } from '@gateway/services/api/order.service';
 // import { axiosReviewInstance } from '@gateway/services/api/review.service';
 import { isAxiosError } from 'axios';
 
@@ -78,7 +78,7 @@ export class GatewayServer {
         axiosSellerInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
         axiosGigInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
         axiosMessageInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
-        // axiosOrderInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
+        axiosOrderInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
         // axiosReviewInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
       }
       next();
